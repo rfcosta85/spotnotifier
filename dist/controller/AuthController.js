fc98ba656cb75332fc95ad98c10ef1389f99729f
Rodrigo Costa <rfcosta85@gmail.com>
2023-12-06 11:25:25 -0300

    implementando logs de tentativa de login e log session

--- dist/controller/AuthController.js
+++ dist/controller/AuthController.js
@@ -1,9 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AuthController = void 0;
 const prisma_1 = require("../utils/prisma");
 const bcryptjs_1 = require("bcryptjs");
 const jsonwebtoken_1 = require("jsonwebtoken");
+const logger_1 = __importDefault(require("../libLog/logger"));
 class AuthController {
     async authenticate(req, res) {
         const { email, password } = req.body;
@@ -25,6 +29,14 @@ class AuthController {
         }
         const token = (0, jsonwebtoken_1.sign)({ id: user.id }, secretKey, { expiresIn: "1d" });
         const { id } = user;
+        const logMessage = {
+            timeStamps: new Date().toLocaleDateString(),
+            event: "Tentativa de login",
+            email,
+            success: isValuePassword,
+            ipAddress: req.ip,
+        };
+        logger_1.default.info(JSON.stringify(logMessage));
         return res.json({ user: { id, email }, token });
     }
 }
--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { prisma } from "../utils/prisma";
 import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
+import logger from "../libLog/logger";
 
 export class AuthController {
   async authenticate(req: Request, res: Response) {
@@ -32,6 +33,26 @@ export class AuthController {
     const token = sign({ id: user.id }, secretKey, { expiresIn: "1d" });
 
     const { id } = user;
+
+    const logMessageTentativasDeLogin = {
+      timeStamps: new Date().toLocaleDateString(),
+      event: "Tentativa de login",
+      email,
+      success: isValuePassword,
+      ipAddress: req.ip,
+    };
+
+    logger.info(JSON.stringify(logMessageTentativasDeLogin));
+
+    const logMessageLogSession = {
+      timestamp: new Date().toLocaleDateString(),
+      event: "Session",
+      email,
+      action: "Session Started",
+      ipAddress: req.ip,
+    };
+
+    logger.info(JSON.stringify(logMessageLogSession));
 
     return res.json({ user: { id, email }, token });
   }
